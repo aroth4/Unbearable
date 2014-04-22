ff670f786ec7c3f3cc90d040b6727b90f2ada5cd
hdao2 <hdao2@KEC119-17.ycp.edu>
2014-04-22 15:53:35 -0400

    minor change to the client side implementation, still need to test it

--- Unbearable/src/edu/ycp/cs/cs496/unbearable/MainActivity.java
+++ Unbearable/src/edu/ycp/cs/cs496/unbearable/MainActivity.java
@@ -1,14 +1,26 @@
 package edu.ycp.cs.cs496.unbearable;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.net.URI;
+import java.net.URISyntaxException;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIUtils;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 import edu.ycp.cs.cs496.unbearable.model.Login;
 import edu.ycp.cs.cs496.unbearable.model.json.*;
 import edu.ycp.cs.cs496.unbearable.util.SystemUiHider;
+import edu.ycp.cs.cs496.unbearable.model.json.JSON;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -129,29 +141,41 @@ public class MainActivity extends Activity {
 				
 			}
 			
-			public boolean LoginPost(String username, String password){
+			public boolean LoginPost(String username, String password)throws URISyntaxException, JsonGenerationException, JsonMappingException, IOException{
 		
 				// Create HTTP client
-//				HttpClient client = new DefaultHttpClient();
-//				
-//				JSON.getObjectMapper().writeValue(sw, newItem);
+				HttpClient client = new DefaultHttpClient();
+				// Construct URI
+				URI uri;
+				uri = URIUtils.createURI("http", "10.0.2.2", 8081, "/login/", 
+						    null, null);
 				
 				//loginPost getPost = new loginPost();
 				Login login = new Login();
 				login.setUsername(username);
 				login.setPassword(password);
-//				@Override
-//				public Item addItem(String itemName, int numItem) {
-//					// add item to the inventory
-//					for (Item item : inventory){
-//						if(item.getName().equals(itemName)){
-//							item.setQuantity(numItem);
-//							return item;
-//						}
-//					
-//					}
+				StringWriter sw = new StringWriter();
+				JSON.getObjectMapper().writeValue(sw, login);
+			
+				StringEntity reqEntity = new StringEntity(sw.toString());
+				reqEntity.setContentType("application/json");
+				// Construct request
+				HttpPost request = new HttpPost(uri);
 				
-				return false;
+				// process request
+				request.setEntity(reqEntity);
+				
+				HttpResponse response = client.execute(request);
+				// Parse response
+				if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+					// Copy the response body to a string
+					HttpEntity entity = response.getEntity();
+					
+					return true;
+				}
+				else{	
+					return false;
+				}
 				
 			}
 			
