eaee0cf1a53ef131d4550db393437144f73d9886
aroth4 <aroth4@ycp.edu>
2014-04-23 21:39:56 -0400

    Merge fix

--- Unbearable/src/edu/ycp/cs/cs496/unbearable/MainActivity.java
+++ Unbearable/src/edu/ycp/cs/cs496/unbearable/MainActivity.java
@@ -1,5 +1,29 @@
 package edu.ycp.cs.cs496.unbearable;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import edu.ycp.cs.cs496.unbearable.model.Login;
+import edu.ycp.cs.cs496.unbearable.model.json.JSON;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIUtils;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.ycp.cs.cs496.unbearable.util.SystemUiHider;
+
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -91,26 +115,26 @@ public class MainActivity extends Activity {
 			  		{
 			  			Toast.makeText(MainActivity.this, "Please fill out all fields", Toast.LENGTH_SHORT).show();
 			  		}
-//			  		else
-//			  		{
-//			  			String checkName = username.getText().toString();
-//			  			String checkPass = password.getText().toString();
-//			  			
-//			  			boolean LoginCheck = controller.getLogin(checkName, checkPass);
-//			  			//Toast.makeText(MainActivity.this, checkName + checkPass, Toast.LENGTH_SHORT).show();
-//			  			if(LoginCheck == true)
-//			  			{
-//			  				//Username exists and is correct! Go to game
-//			  				Toast.makeText(MainActivity.this, "Login Successful! Time to game!", Toast.LENGTH_SHORT).show();
-//							startActivity(new Intent(MainActivity.this, GameActivity.class));
-//			  			}
-//			  			else
-//			  			{
-//			  				//username doesn't exist or was input wrong, return area
-//			  				Toast.makeText(MainActivity.this, "Username/Password incorrect or does not exist", Toast.LENGTH_SHORT).show();
-//			  			}
-//			  			
-//			  		}
+			  		else
+			  		{
+			  			String checkName = username.getText().toString();
+			  			String checkPass = password.getText().toString();
+			  			
+			  			boolean LoginCheck = LoginPost(checkName, checkPass);
+			  			//Toast.makeText(MainActivity.this, checkName + checkPass, Toast.LENGTH_SHORT).show();
+			  			if(LoginCheck == true)
+			  			{
+			  				//Username exists and is correct! Go to game
+			  				Toast.makeText(MainActivity.this, "Login Successful! Time to game!", Toast.LENGTH_SHORT).show();
+							startActivity(new Intent(MainActivity.this, GameActivity.class));
+			  			}
+			  			else
+			  			{
+			  				//username doesn't exist or was input wrong, return area
+			  				Toast.makeText(MainActivity.this, "Username/Password incorrect or does not exist", Toast.LENGTH_SHORT).show();
+			  			}
+			  			
+			  		}
 					 
 				}
 				catch (Exception e) {
@@ -118,6 +142,47 @@ public class MainActivity extends Activity {
 				}
 				
 			}
+			
+			public boolean LoginPost(String username, String password)throws URISyntaxException, JsonGenerationException, JsonMappingException, IOException{
+		
+				// Create HTTP client
+				HttpClient client = new DefaultHttpClient();
+				// Construct URI
+				URI uri;
+				uri = URIUtils.createURI("http", "10.0.2.2", 8081, "/login/", 
+						    null, null);
+				
+				//loginPost getPost = new loginPost();
+				Login login = new Login();
+				login.setUsername(username);
+				login.setPassword(password);
+				StringWriter sw = new StringWriter();
+				JSON.getObjectMapper().writeValue(sw, login);
+			
+				StringEntity reqEntity = new StringEntity(sw.toString());
+				reqEntity.setContentType("application/json");
+				// Construct request
+				HttpPost request = new HttpPost(uri);
+				
+				// process request
+				request.setEntity(reqEntity);
+				
+				HttpResponse response = client.execute(request);
+				// Parse response
+				if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+					// Copy the response body to a string
+					HttpEntity entity = response.getEntity();
+					
+					return true;
+				}
+				else{	
+					return false;
+				}
+				
+			}
+			
+			
+
 		});
         
         // TODO: Set onClickListeners for buttons
